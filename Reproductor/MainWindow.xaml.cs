d623f51341c074f7dd29c494a4e26c3671ecf67c
fer <reikafer@gmail.com>
2019-02-22 12:34:58 -0700

    ReproduceMusica

--- Reproductor/MainWindow.xaml.cs
+++ Reproductor/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
 
 using Microsoft.Win32;
 
@@ -22,9 +24,29 @@ namespace Reproductor
     /// </summary>
     public partial class MainWindow : Window
     {
+        //lee el arvchivo de audio
+        AudioFileReader reader;
+        //dispositivo de salida, responde a los eventos como pausa, play y eso, se comunica con la tarjeta de sonido
+        //nuestra comunicacion con la tarjeta de sonido
+        WaveOutEvent output;
+
         public MainWindow()
         {
             InitializeComponent();
+            LlenarComboSalida();
+        }
+
+        private void LlenarComboSalida()
+        {
+            cbSalida.Items.Clear();
+            for(int i=0; i<WaveOut.DeviceCount; i++)
+            {
+                //da las capacidades disponibles de un dispositivo en especifico
+                WaveOutCapabilities capacidades = WaveOut.GetCapabilities(i);
+                //se añaden al combobox los nombres 
+                cbSalida.Items.Add(capacidades.ProductName);
+            }
+            cbSalida.SelectedIndex = 0;
         }
 
         private void btnElegirArchivo_Click(object sender, RoutedEventArgs e)
@@ -35,6 +57,78 @@ namespace Reproductor
             {
                 txtRutaArchivo.Text =
                      openFileDialog.FileName;
+            }
+        }
+
+        private void btnReproducir_Click(object sender, RoutedEventArgs e)
+        {
+            
+            //evitar duplicado despues del primer play
+            //si ademas de ya tener el output esta pausado
+            if(output!= null && output.PlaybackState == PlaybackState.Paused)
+            {
+                output.Play();
+
+                //activamos y desactivamos los botones para poner restricciones en la interfaz
+                btnReproducir.IsEnabled = false;
+                btnPausa.IsEnabled = true;
+                btnDetener.IsEnabled = true;
+            }
+            else
+            {
+                reader = new AudioFileReader(txtRutaArchivo.Text);
+                output = new WaveOutEvent();
+
+                //aqui se cambia donde se reproduce
+                output.DeviceNumber = cbSalida.SelectedIndex;
+
+                //se añaden reacciones a los eventos con += pues asi se sobrecarga el operador
+                //con tab crea la funcion visual (output_playbackstopped)
+                output.PlaybackStopped += Output_PlaybackStopped;
+                //inisializamos el output
+                output.Init(reader);
+                output.Play();
+
+                //activamos y desactivamos los botones para poner restricciones en la interfaz
+                btnDetener.IsEnabled = true;
+                btnPausa.IsEnabled = true;
+                btnReproducir.IsEnabled = false;
+
+                //se quitn los milisegundos y eso
+                lblTiempoFinal.Text = reader.TotalTime.ToString().Substring(0, 8);
+            }
+            
+        }
+
+        private void Output_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            reader.Dispose();
+            output.Dispose();
+        }
+
+        private void btnPausa_Click(object sender, RoutedEventArgs e)
+        {
+            if(output != null)
+            {
+                output.Pause();
+
+                //activamos y desactivamos los botones para poner restricciones en la interfaz
+                btnDetener.IsEnabled = true;
+                btnPausa.IsEnabled = false;
+                btnReproducir.IsEnabled = true;
+            }
+        }
+
+        private void btnDetener_Click(object sender, RoutedEventArgs e)
+        {
+            if (output!= null)
+            {
+                output.Stop();
+
+                //activamos y desactivamos los botones para poner restricciones en la interfaz
+                btnDetener.IsEnabled = false;
+                btnPausa.IsEnabled = false;
+                btnReproducir.IsEnabled = true;
             }
         }
     }
